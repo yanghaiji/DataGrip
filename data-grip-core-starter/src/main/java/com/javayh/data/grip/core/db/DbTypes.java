eef1bfcf366a9fdab4694e3d41452636ebff4acc
yanghaiji <hai.ji.yang@ibm.com>
2023-06-16 12:45:08 +0800

    init oracle to postgres

---
+++ data-grip-core-starter/src/main/java/com/javayh/data/grip/core/db/DbTypes.java
@@ -0,0 +1,27 @@
+package com.javayh.data.grip.core.db;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author hai ji
+ * @version 1.0.0
+ * @since 2023-06-15
+ */
+public enum DbTypes {
+    VARCHAR2,
+    NVARCHAR2,
+    CHAR,
+    VARCHAR,
+    NUMBER,
+    FLOAT,
+    DECIMAL,
+    DATA_PRECISION,
+    DATA_SCALE,
+    NUMERIC,
+    DATE,
+    TIMESTAMP,
+
+    ;
+}
